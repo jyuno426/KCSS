cf5857464fa4d8fc2cacd5a80fc405596f3b4b00
Junho Han <jyuno426@kaist.ac.kr>
2020-08-13 21:49:49 +0900

    handle static files in production

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
